357658b587ddc4e84f8abed56cfdd12ebc1f883b
Andrew Cooley <andrew.cooley@slalom.com>
2018-03-29 23:24:03 -0500

    Wait for instances to stop and start during snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -128,7 +129,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.excepts.ClientError as e:
+            print(" Cloud not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -142,7 +147,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.excepts.ClientError as e:
+            print(" Cloud not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
